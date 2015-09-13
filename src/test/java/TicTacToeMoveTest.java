f0e946921fb03172ba05e9a5b3ebf2473f7d99ac
Jason <Jason@192.168.1.112>
2015-09-13 13:38:45 -0500

    Added a jtest for TicTacToeMove

---
+++ src/test/java/TicTacToeMoveTest.java
@@ -0,0 +1,29 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TicTacToeMoveTest
+{
+
+	@Test
+	public void testMoveX()
+	{
+		TicTacToeMove move = new TicTacToeMove(1,2,3);
+		assertTrue(move.getX()==2);
+	}
+	
+	@Test
+	public void testMoveY()
+	{
+		TicTacToeMove move = new TicTacToeMove(1,2,3);
+		assertTrue(move.getY()==3);
+	}
+
+	@Test
+	public void testMovePlayer()
+	{
+		TicTacToeMove move = new TicTacToeMove(1,2,3);
+		assertTrue(move.getPlayer()==1);
+	}	
+	
+}
