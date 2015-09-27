9ec780386eaa318aee398d64bf28bbd02601e9a3
Jed Chou <jedchou1@illinois.edu>
2015-09-27 14:13:03 -0500

    fixed AbstractPlayer

--- src/main/java/AbstractPlayer.java
+++ src/main/java/AbstractPlayer.java
@@ -1,6 +1,6 @@
 //  Abstract Player - abstraction with which Referee plays, to separate AI impl
 public abstract class AbstractPlayer {
-  public abstract void receiveState(TicTacToeState state);
+  public abstract void receiveState(GameState state);
   public abstract Move getMove();
   public abstract void newMatch();
 }
