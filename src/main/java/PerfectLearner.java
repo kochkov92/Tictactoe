07e924ab114c74490754af4326a7862742efd9b5
Jed Chou <jedchou1@illinois.edu>
2015-10-18 14:05:41 -0500

    add loadBrain file

--- src/main/java/PerfectLearner.java
+++ src/main/java/PerfectLearner.java
@@ -1,10 +1,12 @@
+import java.io.*;
+
 public class PerfectLearner extends AbstractPlayer
 {
   TicTacToeMove nextMove;
   double[][] qValues;
-
+  int size = (int) Math.pow(3,9);
+    
   public PerfectLearner(int playerNumber) {
-    int size = Math.pow(3,9);
     qValues = new double[size][9];
     for (int i = 0; i < size; i++) {
       for (int j = 0; j < 9; j++) {
@@ -15,22 +17,38 @@ public class PerfectLearner extends AbstractPlayer
 
   public int findIndex(TicTacToeState state) {
     int index = 0;
+    return 7;
   }
 
-  public void saveBrain();
-  public void loadBrain();
-  public void play_Learn();
-
-  @Override
-  public Move getMove();
-
-  @Override
-  public void newMatch();
-
-  @Override
-  public void receiveState(GameState state);
-
-
-  int findIndex(TicTacToeState state) {
-
+  public void saveBrain(){
+      try {
+	  File file = new File("brain.csv");
+	  FileWriter fw = new FileWriter(file.getAbsoluteFile());
+	  BufferedWriter bw = new BufferedWriter(fw);
+	  for (int i = 0; i < size; i++){
+	      bw.write(i);
+	      for (int j = 0; j < 9; j++){
+		  bw.write("," + qValues[i][j]);
+	      }
+	      bw.write("\n");
+	  }
+      } catch (IOException e){
+	  e.printStackTrace();
+      }
   }
+
+  
+    public void loadBrain(){}
+    public void play_Learn(){}
+
+  @Override
+  public Move getMove(){
+      return null;
+  }
+
+  @Override
+  public void newMatch(){}
+
+  @Override
+  public void receiveState(GameState state){}
+}
