83065751bd4d167f0465403f4d84619010b437fa
Dima <kochkov92@gmail.com>
2015-10-25 12:50:57 -0500

    some changes to Qlearner

--- src/main/java/PerfectLearner.java
+++ src/main/java/PerfectLearner.java
@@ -2,6 +2,10 @@ public class PerfectLearner extends AbstractPlayer
 {
   TicTacToeMove nextMove;
   double[][] qValues;
+  double learningRate;
+  double discountFactor;
+  int previousIndex;
+  int previousMove;
 
   public PerfectLearner(int playerNumber) {
     int size = Math.pow(3,9);
@@ -13,8 +17,45 @@ public class PerfectLearner extends AbstractPlayer
     }
   }
 
+  //  set learning parameteres
+  public void setParameters(double learning_rate, discount_factor) {
+    learningRate = learning_rate;
+    discountFactor = discount_factor;
+  }
+
+  //  returns an index of the state in the qValue array
   public int findIndex(TicTacToeState state) {
     int index = 0;
+    for (int i = 0; i < state.sizeX; i++) {
+      for (int j = 0; j < state.stateY; j++) {
+        index = 3 * index + state.board[i][j];
+      }
+    }
+    return index;
+  }
+
+  public void updateQ(TicTacToeState state) {
+    qValues[previousIndex][previousMove] = qValues[previousIndex][previousMove]
+      + learningRate * ( reward(previousIndex) + discountFactor * 
+        maxQDiff(state, previousIndex, previousMove));
+  }
+
+  private double reward(previousIndex) {
+    return -0.01;
+  }
+
+  public double maxQDiff(TicTacToeState state, int previousIndex,
+    int previousMove) {
+    double best = qValues[findIndex(state)][0] -
+      qValues[previousIndex][previousMove];
+    for (int i = 0; i < 9; i++) {
+      if ((qValues[findIndex(state)[i]] - 
+        qValues[previousIndex][previousMove]) > best) {
+        best = (qValues[findIndex(state)[i]] - 
+        qValues[previousIndex][previousMove]);
+      }
+    }
+    return best;
   }
 
   public void saveBrain();
